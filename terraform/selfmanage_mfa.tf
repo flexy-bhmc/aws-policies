acc79e7c22df91c01ea96d222ed43ccbf3ba1c89
DOMAIN\daniel.yosifov <daniel.yosifov@softwareone.com>
2022-12-16 11:42:26 +0200

    terraform IAM policy resource

--- terraform/selfmanage_mfa.tf
+++ terraform/selfmanage_mfa.tf
@@ -1,47 +1,51 @@
-#!/bin/bash 
+provider "aws" {
+}
 
-echo "Creating IAM policy 'iam_policy'"
-aws iam create-policy --policy-name "iam_policy" \
- --description "An IAM policy that allows IAM users to self-manage an MFA device. This policy provides the permissions necessary to complete this action using the AWS API or AWS CLI only." \
- --policy-document '{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "iam:CreateVirtualMFADevice",
-                "iam:EnableMFADevice",
-                "iam:ResyncMFADevice",
-                "iam:DeleteVirtualMFADevice"
-            ],
-            "Resource": [
-                "arn:aws:iam::*:mfa/${aws:username}",
-                "arn:aws:iam::*:user/${aws:username}"
-            ],
-            "Effect": "Allow"
-        },
-        {
-            "Action": [
-                "iam:DeactivateMFADevice"
-            ],
-            "Resource": [
-                "arn:aws:iam::*:mfa/${aws:username}",
-                "arn:aws:iam::*:user/${aws:username}"
-            ],
-            "Effect": "Allow",
-            "Condition": {
-                "Bool": {
-                    "aws:MultiFactorAuthPresent": "true"
-                }
-            }
-        },
-        {
-            "Action": [
-                "iam:ListMFADevices",
-                "iam:ListVirtualMFADevices",
-                "iam:ListUsers"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
+resource "aws_iam_policy" "IamPolicy" {
+  description = "An IAM policy that allows IAM users to self-manage an MFA device. This policy provides the permissions necessary to complete this action using the AWS API or AWS CLI only."
+  policy = jsonencode(
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "iam:CreateVirtualMFADevice",
+        "iam:EnableMFADevice",
+        "iam:ResyncMFADevice",
+        "iam:DeleteVirtualMFADevice"
+      ],
+      "Resource": [
+        "arn:aws:iam::*:mfa/${aws:username}",
+        "arn:aws:iam::*:user/${aws:username}"
+      ],
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "iam:DeactivateMFADevice"
+      ],
+      "Resource": [
+        "arn:aws:iam::*:mfa/${aws:username}",
+        "arn:aws:iam::*:user/${aws:username}"
+      ],
+      "Effect": "Allow",
+      "Condition": {
+        "Bool": {
+          "aws:MultiFactorAuthPresent": "true"
         }
-    ]
-}'
+      }
+    },
+    {
+      "Action": [
+        "iam:ListMFADevices",
+        "iam:ListVirtualMFADevices",
+        "iam:ListUsers"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+)
+
+}
